e5a987c18ea10fbd32e93ae5eed110b586f693ff
john zhang <jzhang@forio.com>
2015-11-04 16:21:30 -0800

    use faster video compile

--- rtc-recording/node/video-processor.js
+++ rtc-recording/node/video-processor.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var exec = require('child_process').exec;
 var uuid = require('node-uuid');
 var videoFileExtension = '.webm';
+var audioFileExtension = '.wav';
 var blobs = [];
 var filePath = './uploads/';
 
@@ -22,8 +23,8 @@ function writeOrAppendData(data, fileName, fileType, videoCounter, ws) {
 
 function fixWebmAudio(fileName, callback) {
     var file = filePath + fileName + videoFileExtension;
-    var movFile = filePath + fileName + '.mov';
-    var ffmpegcommand = 'ffmpeg -i ' + file + ' -c:v prores -c:a pcm_s16le ' + movFile;
+    var wavFile = filePath + fileName + audioFileExtension;
+    var ffmpegcommand = 'ffmpeg -i ' + file + ' -c:a pcm_s16le ' + wavFile;
     exec(ffmpegcommand, { maxBuffer: 20000 * 1024 }, function (error, stdout, stderr) {
         if (error) {
             console.log(error);
@@ -35,7 +36,12 @@ function fixWebmAudio(fileName, callback) {
 
 function concatVideoes(fileNames) {
     console.log('trying to concat videoes');
-    var ffmpegcommand = 'ffmpeg -i ' + filePath + fileNames[0] + '.mov' +' -i ' + filePath + fileNames[1] + '.mov' + ' -filter_complex "[0:v]scale=iw/2:ih/2,pad=2*iw:ih[left];[1:v]scale=iw/2:ih/2[right];[left][right]overlay=w[out];[0:a][1:a]amerge=inputs=2[a]" -map "[out]" -map "[a]" ' + filePath + 'concated-videos' + videoFileExtension;
+    var ffmpegcommand = 'ffmpeg -i '
+        + filePath + fileNames[0] + videoFileExtension
+        + ' -i ' + filePath + fileNames[1] + videoFileExtension +
+        + ' -i ' + filePath + fileNames[0] + audioFileExtension +
+        + ' -i ' + filePath + fileNames[1] + audioFileExtension +
+        ' -filter_complex "[0:v]scale=iw/2:ih/2,pad=2*iw:ih[left];[1:v]scale=iw/2:ih/2[right];[left][right]overlay=w[out];[2:a][3:a]amerge=inputs=2[a]" -map "[out]" -map "[a]" ' + filePath + 'concated-videos' + videoFileExtension;
     exec(ffmpegcommand, { maxBuffer: 20000 * 1024 }, function (error, stdout, stderr) {
         if (error) {
             console.log(error);
@@ -43,7 +49,6 @@ function concatVideoes(fileNames) {
             console.log('concat finished');
         }
     });
-
 };
 
 module.exports = function (app) {
